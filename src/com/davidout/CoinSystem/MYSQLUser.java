3ebb1d051cf73eef4bfd6c3a236d48093e1ea02b
David <davidoutdeveloper@gmail.com>
2022-06-01 22:52:11 +0200

    Delete MYSQLUser.java

--- src/com/davidout/CoinSystem/MYSQLUser.java
+++
@@ -1,9 +0,0 @@
-package com.davidout.CoinSystem;
-
-public class MYSQLUser {
-
-    public static String host = "162.33.28.131";
-    public static String databaseName = "mc142855";
-    public static String username = "mc142855";
-    public static String password = "3fb7331814";
-}
